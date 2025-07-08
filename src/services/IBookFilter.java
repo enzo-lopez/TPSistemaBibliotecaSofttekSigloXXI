e73be6115deaa5ca71ec4223cf5b60d355752296
Agustín <agus.aguirre.pro@gmail.com>
2025-07-08 11:24:00 -0300

    Comentario agregado

--- src/services/IBookFilter.java
+++ src/services/IBookFilter.java
@@ -5,7 +5,7 @@ import java.util.List;
 import entities.Book;
 
 public interface IBookFilter {
-	
+	// Comentario por Agustín Aguirre
 	public List<Book> autorFilter(List<Book> books, String autor);
 	public List<Book> anioFilter(List<Book> books, int year);
 	public List<Book> disponibilidadFilter(List<Book> books);
